220e4ef6a3ade39d3bad931c89b17e1b52846461
marcelorvp <ipmicrobious@gmail.com>
2016-11-04 20:55:05 +0000

    Conditional on monitorMode

--- WifiCracker.sh
+++ WifiCracker.sh
@@ -1,5 +1,5 @@
 # Programa en bash hecho para averiguar la contraseña de un Wifi por medio de
-# deautenticación de clientes conectados a la misma - (https://github.com/Marcelorvp/networkAttack)
+# deautenticación de clientes conectados a la misma - (https://github.com/Marcelorvp/WifiCracker)
 
 # Copyright (c) 2016 Marcelo Raúl Vázquez Pereyra
 
@@ -19,22 +19,28 @@ monitorMode(){
   echo " "
   echo "Iniciando modo monitor..."
   sleep 2
-  airmon-ng start wlp2s0
-  echo " "
-  echo "Dando de baja la interfaz mon0"
-  echo " "
-  sleep 2
-  ifconfig mon0 down
-  echo "Cambiando direccion MAC..."
-  echo " "
-  sleep 2
-  macchanger -a mon0
-  echo " "
-  echo "Dando de alta la interfaz mon0"
-  echo " "
-  sleep 2
-  ifconfig mon0 up
-
+  if [ "ifconfig | grep $monitor" = "$monitor" ]; then
+    echo " "
+    echo "Ya iniciaste antes el modo monitor, no se te va a permitir iniciar otro"
+    echo " "
+    sleep 3
+  else
+    airmon-ng start wlp2s0
+    echo " "
+    echo "Dando de baja la interfaz mon0"
+    echo " "
+    sleep 2
+    ifconfig mon0 down
+    echo "Cambiando direccion MAC..."
+    echo " "
+    sleep 2
+    macchanger -a mon0
+    echo " "
+    echo "Dando de alta la interfaz mon0"
+    echo " "
+    sleep 2
+    ifconfig mon0 up
+  fi
 }
 
 interfacesMode(){
